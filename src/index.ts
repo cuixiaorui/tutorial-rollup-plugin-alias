1e6aab43d5ba0a8c47e7d757d2966ab21def6d0c
cuixiaorui <cui_xiaorui@126.com>
2022-12-21 13:39:24 +0800

    add types & support RegExp

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+interface Alias {
+  find: string | RegExp;
+  replacement: string;
+}
+
+interface Options {
+  entries: Alias[] | { [find: string]: string };
+}
+
+const alias = (options: Options) => {
+  const entries = normalizeEntries(options);
+
+  return {
+    resolveId(source: string, importer: string | undefined) {
+      for (const entry of entries) {
+        const { find, replacement } = entry;
+        if (entry.match(source)) {
+          return source.replace(find, replacement);
+        }
+      }
+    },
+  };
+};
+
+class Entry {
+  find: string | RegExp;
+  replacement: string;
+  constructor(find: string | RegExp, replacement: string) {
+    this.find = find;
+    this.replacement = replacement;
+  }
+  match(source: string) {
+    if (typeof this.find === "string") {
+      return source.startsWith(this.find + "/");
+    } else {
+      return this.find.test(source);
+    }
+  }
+}
+
+function normalizeEntries(options: Options): Entry[] {
+  const {entries} = options
+  if (Array.isArray(entries)) {
+    return entries.map(({ find, replacement }) => {
+      return new Entry(find, replacement);
+    });
+  }
+
+  return Object.entries(entries).map(([find, replacement]) => {
+    return new Entry(find, replacement);
+  });
+}
+
+export { alias };
