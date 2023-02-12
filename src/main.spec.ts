21604fd6c4f6a242774aaba43a83edfb921622ad
cuixiaorui <cui_xiaorui@126.com>
2023-02-12 23:07:26 +0800

    add the realization of the second version

---
+++ src/main.spec.ts
@@ -0,0 +1,63 @@
+import { describe, it, expect } from "vitest";
+import { alias } from "./main";
+
+describe("entries is object", () => {
+  it("should replace alias value when match", () => {
+    const aliasObj: any = alias({
+      entries: {
+        "@": "./src/utils",
+        utils: "./src/utils",
+      },
+    });
+
+    expect(aliasObj.resolveId("@/add")).toBe("./src/utils/add.js");
+    expect(aliasObj.resolveId("utils/add")).toBe("./src/utils/add.js");
+  });
+
+  it("should does not replace alias value when does not match", () => {
+    const aliasObj: any = alias({
+      entries: {
+        "@": "./src/utils",
+      },
+    });
+
+    expect(aliasObj.resolveId("./src/utils/add.js")).toBe("./src/utils/add.js");
+  });
+});
+
+describe("entries is array", () => {
+  it("should replace alias value when match", () => {
+    const aliasObj: any = alias({
+      entries: [
+        {
+          find: "@",
+          replacement: "./src/utils",
+        },
+        {
+          find: "utils",
+          replacement: "./src/utils",
+        },
+      ],
+    });
+
+    expect(aliasObj.resolveId("@/add")).toBe("./src/utils/add.js");
+    expect(aliasObj.resolveId("utils/add")).toBe("./src/utils/add.js");
+  });
+  it("should replace alias value when find is RegExp", () => {
+    const aliasObj: any = alias({
+      entries: [{ find: /^(.*)\.js$/, replacement: "$1.alias" }],
+    });
+
+    expect(aliasObj.resolveId("add.js")).toBe("add.alias.js");
+  });
+
+  it("should does not replace alias value when does not match", () => {
+    const aliasObj: any = alias({
+      entries: {
+        "@": "./src/utils",
+      },
+    });
+
+    expect(aliasObj.resolveId("./src/utils/add.js")).toBe("./src/utils/add.js");
+  });
+});
